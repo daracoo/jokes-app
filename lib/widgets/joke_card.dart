809b6cfd4fc80d479acfa314296406110ed8402f
daracoo <dkuzmanoski21@gmail.com>
2024-12-12 00:21:38 +0100

    Added api & model card

---
+++ lib/widgets/joke_card.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class JokeCard extends StatelessWidget {
+  final String type;
+  final VoidCallback onPress;
+
+  const JokeCard({required this.type, required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            type,
+            style: const TextStyle(fontSize: 21),
+          ),
+        ),
+      ),
+    );
+  }
+}
